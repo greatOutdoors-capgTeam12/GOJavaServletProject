b7ff82024b3c72bd4c234e443eac987b9e24625e
Kunal Roychoudhury <lanuks97@gmail.com>
2019-10-05 12:02:13 +0530

    added presentation to servlet responses as well

--- src/main/java/com/capgemini/go/servlet/DeliveryTimeReportServlet.java
+++ src/main/java/com/capgemini/go/servlet/DeliveryTimeReportServlet.java
@@ -49,10 +49,54 @@ public class DeliveryTimeReportServlet extends HttpServlet {
 			if (result.size() == 0 || result == null) {
 				out.println("No Report To Print");
 			} else {
+				out.print("<!DOCTYPE html>\r\n" + 
+						"<html lang=\"en\">\r\n" + 
+						"    <head>\r\n" + 
+						"        <!-- META DATA -->\r\n" + 
+						"        <meta charset=\"UTF-8\">\r\n" + 
+						"        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\r\n" + 
+						"        <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\r\n" + 
+						"        \r\n" + 
+						"        <!-- TITLE -->\r\n" + 
+						"        <title>Document</title>\r\n" + 
+						"        \r\n" + 
+						"        <!-- Linking bootstrap4 css file from cloud -->\r\n" + 
+						"        <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\r\n" + 
+						"        \r\n" + 
+						"        <!-- Linking bootstrap4 js library files from cloud -->\r\n" + 
+						"        <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>\r\n" + 
+						"        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js\" integrity=\"sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1\" crossorigin=\"anonymous\"></script>\r\n" + 
+						"        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js\" integrity=\"sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM\" crossorigin=\"anonymous\"></script>\r\n" + 
+						"    </head>\r\n" + 
+						"    <body>\r\n" + 
+						"        <div class=\"jumbotron\">\r\n" + 
+						"            <div class=\"table-responsive\">\r\n" + 
+						"                <table class=\"table\">\r\n" + 
+						"                    <thead class=\"thead-dark\">\r\n" + 
+						"                        <tr>\r\n" + 
+						"                            <th>Retailer Id</th>\r\n" + 
+						"                            <th>Product Category</th>\r\n" + 
+						"                            <th>Product UIN</th>\r\n" + 
+						"                            <th>Product Delivery Time (days)</th>\r\n" + 
+						"                        </tr>\r\n" + 
+						"                    </thead>\r\n" + 
+						"                    <tbody>"
+						);
 				for (RetailerInventoryBean r : result) {
-					out.println("<br>");
-					out.println(r.toString());
+					out.print("<tr>");
+					out.print("<td>" + r.getRetailerUserId() + "</td>");
+					out.print("<td>" + r.getProductCategory() + "</td>");
+					out.print("<td>" + r.getProductUIN() + "</td>");
+					out.print("<td>" + r.getProductDeliveryTimePeriod().getDays() + "</td>");
+					out.print("</tr>");
 				}
+				out.print("</tbody>\r\n" + 
+						"                </table>\r\n" + 
+						"            </div>\r\n" + 
+						"        </div>\r\n" + 
+						"    </body>\r\n" + 
+						"</html>"
+						);
 			}
 		} catch (ConnectException exception) {
 			out.println("Error in Retreiving Report >> " + exception.getMessage());
