5b23d678163a71daf15e043250757a47c46116bd
AMANRAJ98 <amanraj98@gmail.com>
2019-10-09 17:31:17 +0530

    aman

--- src/test/java/loginTest/Runner.java
+++ src/test/java/loginTest/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class) 
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber"},monochrome = true)
  
 public class Runner {
 
--- src/test/java/registrationTest/Runner.java
+++ src/test/java/registrationTest/Runner.java
@@ -5,9 +5,9 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class) 
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
- 
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "pretty", "html:target/cucumber" }, monochrome = true)
+
 public class Runner {
 
 }
