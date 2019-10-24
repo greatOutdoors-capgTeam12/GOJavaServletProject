c9feabe12e5ced59ce91a8371e25c3ca20dfa73d
Anshu Kumar <35813504+anshuku@users.noreply.github.com>
2019-10-24 11:01:16 +0530

    update ordprodmap

--- GOJavaServletProject/src/main/java/com/capgemini/go/entity/OrderProductMapPK.java
+++ GOJavaServletProject/src/main/java/com/capgemini/go/entity/OrderProductMapPK.java
@@ -1,9 +1,13 @@
 package com.capgemini.go.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.*;
 
 @Embeddable
-public class OrderProductMapPK {
+public class OrderProductMapPK implements Serializable{
+
+	private static final long serialVersionUID = -3282093658803237026L;
 
 	@Id
 	@Column(name = "order_id", unique = false, length = 20)
